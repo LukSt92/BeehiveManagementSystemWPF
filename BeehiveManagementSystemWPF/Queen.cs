fe8b3499278eb6070cf2f278460e92858be11cfb
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-07-04 07:55:56 +0200

    started building Queen class.

--- BeehiveManagementSystemWPF/Queen.cs
+++ BeehiveManagementSystemWPF/Queen.cs
@@ -8,5 +8,11 @@ namespace BeehiveManagementSystemWPF
 {
     class Queen : Bee
     {
+        public Queen() : base("Queen")
+        { }
+        public const float EGGS_PER_SHIFT = 0.45f;
+        public const float HONEY_PER_UNASSIGNED_WORKER = 0.5f;
+        public string StatusReport { get; private set; }
+        public override float CostPerShift { get { return 2.15f; } }
     }
 }
