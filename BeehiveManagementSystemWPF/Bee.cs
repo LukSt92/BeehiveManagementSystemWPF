b8a2676b6bc7e49a57d06ed986488d5f50e20882
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-07-04 07:38:50 +0200

    added Bee base class.

---
+++ BeehiveManagementSystemWPF/Bee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystemWPF
+{
+    class Bee
+    {
+        public Bee(string job)
+        {
+            Job = job;
+        }
+        public virtual float CostPerShift { get; }
+        public string Job { get; private set; }
+        public void WorkTheNextShift()
+        {
+            if (HoneyVault.ConsumeHoney(CostPerShift))
+                DoJob();
+        }
+        protected virtual void DoJob() { }
+    }
+}
